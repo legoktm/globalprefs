647c67cc5278e7fc0bff85ef17043006cf2f9c44
Kunal Mehta <legoktm@gmail.com>
2014-01-13 20:42:17 -0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+
+import json
+from flask import Flask, request, url_for
+from flask_mwoauth import MWOAuth
+from werkzeug.contrib.cache import FileSystemCache
+
+import config
+
+app = Flask(__name__)
+app.secret_key = config.secret_key
+
+cache = FileSystemCache('cache')
+
+mwoauth = MWOAuth(consumer_key=config.oauth_key, consumer_secret=config.oauth_secret)
+app.register_blueprint(mwoauth.bp)
+
+
+def embed_json(name, data):
+    # https://stackoverflow.com/questions/9320427/best-practice-for-embedding-arbitrary-json-in-the-dom
+    return '<script type="application/json" id="{0}">{1}</script>'.format(name, json.dumps(data))
+
+
+def get_languages():
+    languages = cache.get('languages')
+    if languages is None:
+        data = mwoauth.request({'action': 'query', 'meta': 'siteinfo', 'siprop': 'languages'})
+        languages = {}
+        for lang in data['query']['languages']:
+            languages[lang['code']] = lang['*']
+            cache.set('languages', languages)
+    return languages
+
+
+def get_attached_wikis(username):
+    #meta=globaluserinfo&guiuser=Catrope&guiprop=merged&format=jsonfm
+    wikis = mwoauth.request({
+        'action': 'query',
+        'meta': 'globaluserinfo',
+        'guiuser': username,
+        'guiprop': 'merged'
+    })  # Shouldn't matter what wiki this request runs on
+    for wiki in wikis['query']['globaluserinfo']['merged']:
+        yield wiki['url']
+
+
+@app.route('/api')
+def api():
+    wiki = request.args['wiki'].replace('http:', 'https://') + '/w'
+    value = request.args['value']
+    token = mwoauth.request({'action': 'tokens', 'type': 'options'}, wiki)['tokens']['optionstoken']
+    d = mwoauth.request({'action': 'options', 'token': token, 'change': 'language={0}'.format(value)}, wiki)
+    return json.dumps(d)
+
+
+@app.route('/')
+def index():
+    t = ''
+    username = mwoauth.get_current_user(False)
+    if username:
+        t += 'You are currently logged in as {0}. <a href="logout">Logout</a>'.format(username)
+    else:
+        t += 'You are not logged in. <a href="login">Login</a>.'
+        return t
+    t += '\n<br />\n'
+    t += 'Language: <select name="lang" id="lang-select">\n'
+    languages = get_languages()
+    for lang in languages:
+        t += '\t<option value="{0}">{1}</option>\n'.format(lang, languages[lang])
+    t += '</select>\n'
+    t += '<br />'
+    t += '<button name="go" id="button-go">Go!</button>'
+    t += '<div id="logging"></div>'
+    # Embed some JSON...
+    wikis = {'wikis': list(get_attached_wikis(username))}
+    t += embed_json('attached-wikis', wikis)
+    t += '<script src="//tools.wmflabs.org/static/js/jquery-2.0.3.min.js">'
+    link = url_for('static', filename='js.js')
+    t += '<script src="{0}">'.format(link)
+
+    return t
+
+if __name__ == '__main__':
+    app.run()
---
+++ static/js.js
@@ -0,0 +1,18 @@
+$('#button-go').on('click', function(e){
+	var lang, data, $log;
+	e.preventDefault();
+	lang = $('#lang-select').val();
+	data = JSON.parse($('#attached-wikis').val());
+	$log = $('#logging');
+	$.each(data.wikis, function(index, value){
+		$.ajax({
+			url: location.protocol + '//tools.wmflabs.org/globalprefs/api/',
+			dataType: 'json',
+			async: false,
+			data: {'wiki': value, 'value': lang},
+			success: function(data) {
+				$log.append('Set value on ' + value);
+			}
+		});
+	});
+});
